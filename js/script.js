373cc270985a7d292d4ba709568c2f4e6bf57a6d
arctikbear <coder.arctik@gmail.com>
2021-12-05 18:34:50 +0300

    unit 33

--- js/script.js
+++ js/script.js
@@ -1,20 +1,86 @@
-// console.log(document.head);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+'use strict';
 
-// console.log(document.querySelector('#current').parentNode);
-// console.log(document.querySelector('#current').parentElement);
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]'),
+          button = document.querySelector('button');
+        
+    const createMovieList = (movies, parent) => {
+        parent.innerHTML = '';
+        sortArr(movies);
+        
+        movies.forEach( (movie, i) => {
+            parent.innerHTML +=
+            `<li class="promo__interactive-item">${i + 1} ${movie}
+                <div class="delete"></div>
+            </li>`;
+        });
 
-// console.log(document.querySelector('#current').parentNode);
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(movies, parent);
+            });
+        });
+    };
 
-// console.log(document.querySelector('[data-current="3"]').nextSibling);
-console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+    const deleteAdv = (arr) => {
+        arr.forEach(img => {
+            img.remove();
+        });
+    };
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
-}
+    const makeChanges = () => {
+        genre.textContent = 'Драма';
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        let newFilm = addInput.value;
+        const favourite = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = newFilm.slice(0, 21) + '...';
+            }
+
+            if (favourite) {
+                console.log('movie is favourite');
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);            
+        }
+
+        // addForm.reset(); есть более продвинутая запись:
+        event.target.reset();
+    });
+
+    deleteAdv(adv);
+    sortArr(movieDB.movies);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
+});
